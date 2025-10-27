a7f77f8b450040f2784d737c7feb82a3531cc4a3
Pritam <pritamach.exe@gmail.com>
2025-10-27 13:15:53 +0530

    decoder done. b+ tree creation started

--- core/btree.go
+++ core/btree.go
@@ -44,7 +44,7 @@ func checkLimit(key, val []byte) error {
 	return nil
 }
 
-// Getter & setter header
+// Getter & setter header for decoding on the fly
 
 func (node BNode) btype() uint16 {
 	return binary.LittleEndian.Uint16(node[0:2])
@@ -59,3 +59,91 @@ func (node BNode) setHeader(btype uint16, nkeys uint16) {
 	binary.LittleEndian.PutUint16(node[2:4], nkeys)
 }
 
+func (node BNode) getPtr(idx uint16) uint64 {
+	if idx >= node.nkeys() {
+		panic("getPtr idx out of range")
+	}
+
+	pos := 4 + int(idx)*8 // first 4 bytes are the headers (0-1 are btype and 2-3 are nkeys) and idx*8 each pointer is 8 byptes long i.e. uint64
+
+	return binary.LittleEndian.Uint64(node[pos : pos+8])
+}
+
+func (node BNode) setPtr(idx uint16, val uint64) {
+	if idx >= node.nkeys() {
+		panic("setPtr idx out of range")
+	}
+
+	pos := 4 + int(idx)*8
+	binary.LittleEndian.PutUint64(node[pos:pos+8], val)
+}
+
+func (node BNode) getOffset(idx uint16) uint16 {
+	if idx == 0 {
+		return 0
+	}
+
+	n := node.nkeys()
+	if idx > n {
+		panic("offset idx > nkeys")
+	}
+
+	pos := 4 + int(n)*8 + int(idx-1)*2
+	return binary.LittleEndian.Uint16(node[pos : pos+2])
+}
+
+func (node BNode) setOffset(idx uint16, v uint16) {
+	n := node.nkeys()
+	pos := 4 + int(n)*8 + int(idx-1)*2
+	binary.LittleEndian.PutUint16(node[pos:pos+2], v)
+}
+
+func (node BNode) kvPos(idx uint16) uint16 {
+	n := node.nkeys()
+	return uint16(4+int(n)*8+int(n)*2) + node.getOffset(idx)
+}
+
+func (node BNode) getKey(idx uint16) []byte {
+	if idx >= node.nkeys() {
+		return nil
+	}
+
+	pos := node.kvPos(idx)
+	klen := binary.LittleEndian.Uint16(node[pos : pos+2])
+	return node[pos+4 : pos+4+uint16(klen)]
+}
+
+func (node BNode) getVal(idx uint16) []byte {
+	if idx >= node.nkeys() {
+		return nil
+	}
+
+	pos := node.kvPos(idx)
+	klen := binary.LittleEndian.Uint16(node[pos : pos+2])
+	vlen := binary.LittleEndian.Uint16(node[pos+2 : pos+4])
+
+	return node[pos+4+uint16(klen) : pos+4+uint16(klen)+uint16(vlen)]
+}
+
+func (node BNode) nbytes() uint16 {
+	return node.kvPos(node.nkeys())
+}
+
+func nodeAppendKV(new BNode, idx uint16, ptr uint64, key []byte, val []byte) {
+	new.setPtr(idx, ptr)
+	pos := new.kvPos(idx)
+	binary.LittleEndian.PutUint16(new[pos+0:pos+2], uint16(len(key)))
+	binary.LittleEndian.PutUint16(new[pos+2:pos+4], uint16(len(val)))
+	copy(new[pos+4:], key)
+	copy(new[pos+4+uint16(len(key)):], val)
+	new.setOffset(idx+1, new.getOffset(idx)+uint16(4+len(key)+len(val)))
+}
+
+
+func nodeAppendRange(new BNode, old BNode, dstNew uint16, scrOld uint16, n uint16) {
+	for i := uint16(0); i < n; i++ {
+		dst := dstNew + i
+		src := scrOld + i
+		nodeAppendKV(new, dst, old.getPtr(src), old.getKey(src), old.getVal(src))
+	}
+}
